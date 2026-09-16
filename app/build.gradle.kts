22becf4895f51844c72c05e578b462266c0943d8
Christian <profe.christian.duoc@gmail.com>
2026-09-16 11:40:50 -0300

    Actualizacion de compatibilidad

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "cl.duoc.conociendocompose"
     compileSdk {
-        version = release(37) {
+        version = release(36) {
             minorApiLevel = 1
         }
     }
@@ -29,8 +29,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         compose = true
